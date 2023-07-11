55ce732fe2afdb620df5803c1f6eb957c4dfe1e0
angelDariaux <angeldariaux@gmail.com>
2023-07-10 21:58:41 -0600

    finished the  validations of the negative case

--- 01_DemoQa/__test__/02_Check-Box/03_NegativeCase.test.js
+++ 01_DemoQa/__test__/02_Check-Box/03_NegativeCase.test.js
@@ -53,6 +53,37 @@ describe('Negative Case, missing select buttons', () => {
     await clickDelayed(page, '#tree-node > ol > li > ol > li:nth-child(2) > ol > li:nth-child(2) > ol > li:nth-child(3) > span > label > span.rct-checkbox')
     // Excel File.doc
     await clickDelayed(page, '#tree-node > ol > li > ol > li:nth-child(3) > ol > li:nth-child(2) > span > label > span.rct-checkbox')
-
   }, timeDelay);
+
+  it ('Selecting all the missing options', async () => { 
+
+    // Click on the Home select button
+    await clickDelayed(page, '#tree-node > ol > li > span > label > span.rct-checkbox')
+
+    // Validating that all options were selected: 
+    const expectedTexts = ['home', 'desktop', 'notes', 'commands', 'documents', 'workspace', 'react', 'angular', 'veu', 'office', 'public', 'private', 'classified', 'general', 'downloads', 'wordFile', 'excelFile'];
+
+    for (let i = 0; i < expectedTexts.length; i++) {
+      const selector = `#result > span:nth-child(${i + 2})`;  // +2 because the first span is 'You have selected'
+      const text = await getText(page, selector);
+      expect(text).toBe(expectedTexts[i]);
+    };
+  }, timeDelay);
+
+  it ('Unselecting all the options', async () => { 
+
+    // Click on the Home select button
+    await clickDelayed(page, '#tree-node > ol > li > span > label > span.rct-checkbox')
+
+    // Validating that no files or directories were selected: 
+    const selector = `#result > span:nth-child(1)`;  // This is the 'You have selected' span
+  
+    // Check if the 'You have selected' span exists
+    const spanExists = await page.evaluate((sel) => {
+      return document.querySelector(sel) != null;
+    }, selector);
+  
+    expect(spanExists).toBe(false);  // The span should not exist if no files are selected
+  }, timeDelay)
+
 }, timeDelay)
